45649703c85fdc5df9d5879c6fc68e8b64b54829
luqingxia <1571688206@qq.com>
2023-07-10 22:16:21 +0800

    # 测试

--- Android-SDK@3.3.13.81245_20220315/HBuilder-Integrate-AS/simpleDemo/src/main/java/com/rongjingtaihegezheng/cert/MineTestActivity.java
+++ Android-SDK@3.3.13.81245_20220315/HBuilder-Integrate-AS/simpleDemo/src/main/java/com/rongjingtaihegezheng/cert/MineTestActivity.java
@@ -1,6 +1,7 @@
 package com.rongjingtaihegezheng.cert;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,5 +9,7 @@ public class MineTestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mt);
+        final ImageView imageView = findViewById(R.id.imageView);
     }
 }
